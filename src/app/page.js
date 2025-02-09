3c56182eebbf5ccabe1a8d4a82e2f54f52c43120
Shreya <meenaxjha@gmail.com>
2025-02-09 21:41:59 +0530

    added all the files

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,48 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [response, setResponse] = useState("");
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const API = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+      const genAI = new GoogleGenerativeAI(API);
+      const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+      try {
+        const result = await model.generateContent(search);
+        const text = await result.response.text("");
+        setResponse(text);
+      } catch (error) {
+        console.error("I got an error while fetching the AI data", error);
+      }
+    }
+    fetchData();
+  }, [search]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  //for the form to work
+  function handleSubmit(event) {
+    event.preventDefault();
+    const search = event.target.search.value;
+    console.log('Search:', search);
+    setSearch(`you searched for: ${search}`);
+  }
+
+  return (
+    <div className="bg-stone-900 h-screen mx-auto fixed w-full">
+      <div className="text-center">
+        <div className="py-32 px-32">
+          <p className="text-white text-200 gap-y-20">{response}</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="py-20 relative">
+          <h1 className="text-white text-4xl py-10">How can I help you ?</h1>
+          <form onSubmit={handleSubmit}>
+            <input className="py-2 px-8 h-20 bg-stone-800 text-white w-2/5 my-4" type="text" name="search" placeholder="Ask Me Something" />
+            <button type="submit" className="py-2 h-20 bg-stone-700 text-white  px-6">Search</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
