c26b1953b41a92d0eed552298d7128a1ee5ba453
Shreya <meenaxjha@gmail.com>
2025-02-11 15:29:38 +0530

    added some other things

--- src/app/page.js
+++ src/app/page.js
@@ -1,47 +1,52 @@
 "use client"
-import { useEffect, useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { useState } from "react";
 
 export default function Home() {
   const [response, setResponse] = useState("");
   const [search, setSearch] = useState("");
-  useEffect(() => {
-    async function fetchData() {
-      const API = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
-      const genAI = new GoogleGenerativeAI(API);
-      const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-      try {
-        const result = await model.generateContent(search);
-        const text = await result.response.text("");
-        setResponse(text);
-      } catch (error) {
-        console.error("I got an error while fetching the AI data", error);
-      }
-    }
-    fetchData();
-  }, [search]);
+  const [helpText, setHelpText] = useState("How can I help you ?");
 
-  //for the form to work
+  const ApiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(ApiKey);
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+  const fetchData = async () => {
+    try {
+      const searchResult = await model.generateContent(search);
+      const text = await searchResult.response.text;
+      setResponse(text);
+    } catch {
+      console.error("Error", error);
+      setResponse("failed to get response");
+    }
+  };
+
   function handleSubmit(event) {
     event.preventDefault();
-    const search = event.target.search.value;
-    console.log('Search:', search);
-    setSearch(`you searched for: ${search}`);
+    fetchData();
+    setHelpText(null);
   }
 
+  function handleInput(event) {
+    setSearch(event.target.value);
+  }
   return (
-    <div className="bg-stone-900 h-screen mx-auto fixed w-full">
-      <div className="text-center">
-        <div className="py-32 px-32">
-          <p className="text-white text-200 gap-y-20">{response}</p>
+    <div className="bg-stone-900">
+      <div className="bg-stone-900 z-10 h-full mx-auto w-full">
+        <div className="text-center">
+          <div className="py-32 px-96">
+            <p className="text-white text-200 gap-y-20">{response}</p>
+            <h1 className="text-white text-4xl py-10">{helpText}</h1>
+          </div>
         </div>
-        <div className="py-20 relative">
-          <h1 className="text-white text-4xl py-10">How can I help you ?</h1>
-          <form onSubmit={handleSubmit}>
-            <input className="py-2 px-8 h-20 bg-stone-800 text-white w-2/5 my-4" type="text" name="search" placeholder="Ask Me Something" />
-            <button type="submit" className="py-2 h-20 bg-stone-700 text-white  px-6">Search</button>
-          </form>
-        </div>
+
+      </div>
+      <div className="bg-stone-900 z-40 fixed bottom-0 right-0 left-0 text-center">
+        <form onSubmit={handleSubmit}>
+          <input className="py-2 px-8 h-20 bg-stone-800 text-white w-2/5 my-4" value={search} onChange={handleInput} type="text" placeholder="Ask Me Something" />
+          <button type="submit" name="search" className="py-2 h-20 bg-stone-700 text-white px-6">Search</button>
+        </form>
       </div>
     </div>
   );
