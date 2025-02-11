5bf778768cfb162b7a89878393ee7404549667eb
Shreya <meenaxjha@gmail.com>
2025-02-11 18:16:50 +0530

    improved the destop view

---
+++ src/app/components/sidebar.js
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Sidebar() {
+    return (
+        <div>
+        <div className="h-screen fixed bg-stone-950 w-72 z-40">
+            <div className="py-4 m-2 rounded-xl text-center bg-stone-800 border border-stone-700">
+                <h1 className="text-white">History message</h1>
+            </div>
+            <div className="py-4 m-2 rounded-xl text-center bg-stone-800 border border-stone-700">
+                <h1 className="text-white">History message</h1>
+            </div>
+        </div>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 "use client"
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import Sidebar from "./components/sidebar";
 
 export default function Home() {
   const [response, setResponse] = useState("");
@@ -21,32 +23,45 @@ export default function Home() {
       setResponse("failed to get response");
     }
   };
-
   function handleSubmit(event) {
     event.preventDefault();
     fetchData();
     setHelpText(null);
   }
-
   function handleInput(event) {
     setSearch(event.target.value);
   }
   return (
-    <div className="bg-stone-900">
-      <div className="bg-stone-900 z-10 h-full mx-auto w-full">
-        <div className="text-center">
-          <div className="py-32 px-96">
-            <p className="text-white text-200 gap-y-20">{response}</p>
-            <h1 className="text-white text-4xl py-10">{helpText}</h1>
+    <div className="bg-stone-900 h-screen">
+      <div className="grid grid-cols-5 gap-96">
+        <Sidebar />
+        <div className="col-span-3">
+          <div className="">
+            <div className="w-3/3">
+              <p className="text-white text-200 py-10 ">{response}</p>
+              <h1 className="text-white text-4xl py-10">{helpText}</h1>
+            </div>
+
+            <div className="fixed bottom-0 left-0 right-0 bg-stone-900 z-20">
+              <form className="flex justify-center items-center ml-52" onSubmit={handleSubmit}>
+                <input
+                  className="py-6 px-8 bg-stone-800 text-white w-3/5 my-4 rounded-xl"
+                  value={search}
+                  onChange={handleInput}
+                  type="text"
+                  placeholder="Ask Me Something"
+                />
+                <button
+                  type="submit"
+                  name="search"
+                  className="py-4 bg-stone-700 text-white px-6 flex items-center justify-center rounded-xl ml-2"
+                >
+                  <CiSearch size="40" />
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-
-      </div>
-      <div className="bg-stone-900 z-40 fixed bottom-0 right-0 left-0 text-center">
-        <form onSubmit={handleSubmit}>
-          <input className="py-2 px-8 h-20 bg-stone-800 text-white w-2/5 my-4" value={search} onChange={handleInput} type="text" placeholder="Ask Me Something" />
-          <button type="submit" name="search" className="py-2 h-20 bg-stone-700 text-white px-6">Search</button>
-        </form>
       </div>
     </div>
   );
